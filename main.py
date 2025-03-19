3e920515051f53de72847848e137a7be86eed6a8
doodtato <jeremyowen2009@gmail.com>
2025-03-19 09:40:19 -0500

    added player movability and groups

--- main.py
+++ main.py
@@ -7,23 +7,35 @@ from player import *
 from circleshape import *
 
 def main():
-   pygame.init()
-   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-   clock = pygame.time.Clock()
-   dt = 0
-   player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-   
-   while True:
-       for event in pygame.event.get():
-           if event.type == pygame.QUIT:
-               return
+    pygame.init()
 
-       screen.fill((0,0,0))
-       player1.draw(screen)
-       pygame.display.flip()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+   
+
+
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0,0,0))
+        for object in drawable:
+            object.draw(screen)
+        updateable.update(dt)
+        pygame.display.flip()
        
-       #limit framerate to 60 fps
-       dt = clock.tick(60)/1000
+        #limit framerate to 60 fps
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
