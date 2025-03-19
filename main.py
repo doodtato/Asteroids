c1ef795da64615d776a5af94ab180ee473f7347f
doodtato <jeremyowen2009@gmail.com>
2025-03-18 23:28:15 -0500

    player shape added

--- main.py
+++ main.py
@@ -3,16 +3,27 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
    pygame.init()
    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+   clock = pygame.time.Clock()
+   dt = 0
+   player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+   
    while True:
        for event in pygame.event.get():
            if event.type == pygame.QUIT:
                return
+
        screen.fill((0,0,0))
+       player1.draw(screen)
        pygame.display.flip()
+       
+       #limit framerate to 60 fps
+       dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
